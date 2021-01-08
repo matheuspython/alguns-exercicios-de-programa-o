1c563123848465372318b1db1a1be478d6fda9f0
matheus souza <ruivorobado@gmail.com>
2021-01-08 13:58:58 -0300

    exercicio concluido

---
+++ js/index.js
@@ -0,0 +1,5 @@
+//função que recebe dois numeros e retorna o maior deles
+function retornaMaior(numero1, numero2) {
+    return (numero1 > numero2) ? numero1 : numero2
+}
+console.log(retornaMaior(3, 99))
