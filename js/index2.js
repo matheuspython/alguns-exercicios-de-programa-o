a78a2e73c349fd4f39c50217982d3dfbfb1776f0
matheus souza <ruivorobado@gmail.com>
2021-01-08 14:20:56 -0300

    exercicio 2 concluido

---
+++ js/index2.js
@@ -0,0 +1,9 @@
+//escreva uma função chamada epaisagem que
+//recebe dois argumentos, largura e altura 
+//da imagem ( number )
+//e retorne true se a imagem estiver no modo paisagem
+
+function epaisagem(largura, altura) {
+    return largura >= altura
+}
+console.log(epaisagem(12, 2))
